eedd1e8d7ce6021430af65f1529af8a0f77cb118
Muhammad041008 <mohdimran041008@gmail.com>
2025-07-13 00:50:26 +0530

    Refactor: Replaced use-dark-mode with modern @dark-mode-hook/core for React 18 support

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { useDarkMode } from '@dark-mode-hook/core';;
+import { useDarkMode } from '@dark-mode-hook/core';
 import AppContext from './AppContext';
 import MainApp from './MainApp';
 import GlobalStyles from './theme/GlobalStyles';
